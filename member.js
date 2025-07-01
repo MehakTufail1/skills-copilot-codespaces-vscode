6be01c178012e32c3cbd3492cec875218f6e4aad
Mehak Tufail <2022f-mulmpl-cs-004@mul.edu.pk>
2025-07-01 11:05:32 +0000

    Copilot Third Commit

--- member.js
+++ member.js
@@ -1,1 +1,16 @@
-function skillsMember()
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    description: 'A member with skills',
+    properties: {
+      skills: {
+        type: 'array',
+        items: {
+          type: 'string'
+        },
+        description: 'List of skills the member possesses'
+      }
+    },
+    required: ['skills']
+  };
+}
